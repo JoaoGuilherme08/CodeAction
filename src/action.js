203678eb9a1db3e1b30046ea8d085988e5e0ce6e
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-22 11:28:29 -0300

    a

--- src/action.js
+++ src/action.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fetch = require('node-fetch');
+const fetch = require('whatwg-fetch');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
