3323633009295cccc0bb3c628391b2124541fb73
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-22 17:42:30 -0300

    a

--- dist/index.js
+++ dist/index.js
@@ -8482,46 +8482,42 @@ const github = __nccwpck_require__(5438);
 const fetch = __nccwpck_require__(467);
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
-    var results;
-    var gifUrl;
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const TENOR_TOKEN = core.getInput("TENOR_TOKEN");
+  var results;
 
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+  const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const { context = {} } = github;
-    const { pull_request } = context.payload;  
-    console.log(context.payload);
-    if(pull_request.action == "closed") {
-
-      do{
-        const randomPos = Math.round(Math.random() * 10);
-        const url = `https://api.tenor.com/v1/search?q=recusado&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-        const response = await fetch(url);
-        results = await response.json();
-        gifUrl = results['results'][0]['media'][0]['tinygif']['url'];
-      }while(results['next'] === "0" );
-
-    }else if(pull_request.action == "opened"){
-      do{
-        const randomPos = Math.round(Math.random() * 10);
-        const url = `https://api.tenor.com/v1/search?q=tamo%20junto&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-        const response = await fetch(url);
-        results = await response.json();
-        gifUrl = results['results'][0]['media'][0]['tinygif']['url'];
-      }while(results['next'] === "0" );
-    }
-
-    await octokit.rest.issues.createComment({
-      ...context.repo,
-      issue_number: pull_request.number,
-      body: `Obrigado pela pull request, A Solinftec é a melhor.\n\n<img src="${gifUrl}" alt="thank you" />`
-    });
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+  console.log(context.payload);
+  if (pull_request.action == "closed") {
+    do {
+      const randomPos = Math.round(Math.random() * 10);
+      const url = `https://api.tenor.com/v1/search?q=recusado&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+      const response = await fetch(url);
+      results = await response.json();
+    } while (results["next"] === "0");
+    var gifUrl = results["results"][0]["media"][0]["tinygif"]["url"];
+  } else if (pull_request.action == "opened") {
+    do {
+      const randomPos = Math.round(Math.random() * 10);
+      const url = `https://api.tenor.com/v1/search?q=tamo%20junto&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+      const response = await fetch(url);
+      results = await response.json();
+    } while (results["next"] === "0");
+    var gifUrl = results["results"][0]["media"][0]["tinygif"]["url"];
   }
 
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `Obrigado pela pull request, A Solinftec é a melhor.\n\n<img src="${gifUrl}" alt="thank you" />`,
+  });
+}
 
-  
-  run();
+run();
+
 
 /***/ }),
 
--- src/action.js
+++ src/action.js
@@ -1,45 +1,40 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fetch = require('node-fetch');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fetch = require("node-fetch");
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
-    var results;
-    var gifUrl;
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const TENOR_TOKEN = core.getInput("TENOR_TOKEN");
+  var results;
 
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+  const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const { context = {} } = github;
-    const { pull_request } = context.payload;  
-    console.log(context.payload);
-    if(pull_request.action == "closed") {
-
-      do{
-        const randomPos = Math.round(Math.random() * 10);
-        const url = `https://api.tenor.com/v1/search?q=recusado&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-        const response = await fetch(url);
-        results = await response.json();
-        gifUrl = results['results'][0]['media'][0]['tinygif']['url'];
-      }while(results['next'] === "0" );
-
-    }else if(pull_request.action == "opened"){
-      do{
-        const randomPos = Math.round(Math.random() * 10);
-        const url = `https://api.tenor.com/v1/search?q=tamo%20junto&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-        const response = await fetch(url);
-        results = await response.json();
-        gifUrl = results['results'][0]['media'][0]['tinygif']['url'];
-      }while(results['next'] === "0" );
-    }
-
-    await octokit.rest.issues.createComment({
-      ...context.repo,
-      issue_number: pull_request.number,
-      body: `Obrigado pela pull request, A Solinftec é a melhor.\n\n<img src="${gifUrl}" alt="thank you" />`
-    });
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+  console.log(context.payload);
+  if (pull_request.action == "closed") {
+    do {
+      const randomPos = Math.round(Math.random() * 10);
+      const url = `https://api.tenor.com/v1/search?q=recusado&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+      const response = await fetch(url);
+      results = await response.json();
+    } while (results["next"] === "0");
+    var gifUrl = results["results"][0]["media"][0]["tinygif"]["url"];
+  } else if (pull_request.action == "opened") {
+    do {
+      const randomPos = Math.round(Math.random() * 10);
+      const url = `https://api.tenor.com/v1/search?q=tamo%20junto&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+      const response = await fetch(url);
+      results = await response.json();
+    } while (results["next"] === "0");
+    var gifUrl = results["results"][0]["media"][0]["tinygif"]["url"];
   }
 
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `Obrigado pela pull request, A Solinftec é a melhor.\n\n<img src="${gifUrl}" alt="thank you" />`,
+  });
+}
 
-  
-  run();
+run();
