d3ca26441381c477b64a09b119733f34a6cda268
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-22 11:25:51 -0300

    a

--- dist/index.js
+++ dist/index.js
@@ -14715,30 +14715,35 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const fetch = __nccwpck_require__(9233);
 
-async function run(){
+async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+    var results;
+
+    do{
+      const randomPos = Math.round(Math.random() * 1000);
+      const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+      const response = await fetch(url);
+      results = await response.json();
+    }while(results['next'] === "0" );
+ 
+    const gifUrl = results['results'][0]['media'][0]['tinygif']['url'];
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
-    const { pull_request } = context.payload;
-
-    const randomPos = Math.round(Math.random() * 10);
-    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-    const response = await fetch(url);
-    const { results } = await response.json();
-    const gifUrl = results[0].media[0].tinygif.url;
-
+    const { pull_request } = context.payload;  
+    
     await octokit.rest.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />`
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: `Obrigado pela pull request, vamos analiza-la o mais rapido possivel.\n\n<img src="${gifUrl}" alt="thank you" />`
     });
-}
+  }
 
-run();
+
+  
+  run();
 })();
 
 module.exports = __webpack_exports__;
