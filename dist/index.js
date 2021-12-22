948e89239c0d9b5e9e92591624706dccb8b6b0de
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-22 17:08:28 -0300

    a

--- dist/index.js
+++ dist/index.js
@@ -8488,7 +8488,7 @@ async function run() {
 
     do{
       const randomPos = Math.round(Math.random() * 10);
-      const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+      const url = `https://api.tenor.com/v1/search?q=tamo%20junto&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
       const response = await fetch(url);
       results = await response.json();
     }while(results['next'] === "0" );
